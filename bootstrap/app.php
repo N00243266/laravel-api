2e48a666cfeec54e51baf463e3c206616368febe
Diana <N00243266@iadt.ie>
2026-03-05 21:33:34 +0000

    cors issue fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
 )
     ->withMiddleware(function (Middleware $middleware): void {
 
-        $middleware->append(\App\Http\Middleware\HandleCors::class); // CORS handling for API routes
+      //  $middleware->append(\App\Http\Middleware\HandleCors::class); // CORS handling for API routes
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
