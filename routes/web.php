6f08b44aae87b0e7d16f50e58595690956d10e49
Diana <N00243266@iadt.ie>
2026-03-05 23:11:34 +0000

    Add CORS headers for WebGL

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Response;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/artworks/{file}', function ($file) {
+    $path = public_path('artworks/' . $file);
+
+    if (!file_exists($path)) abort(404);
+
+    return Response::file($path, [
+        'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Allow-Methods' => 'GET, OPTIONS',
+        'Access-Control-Allow-Headers' => 'Origin, Content-Type, Accept, Authorization',
+    ]);
+})->where('file', '.*');
