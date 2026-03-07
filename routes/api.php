9fbe8a4b410ff4c3aed23f355f034f4299d64a8b
Diana <N00243266@iadt.ie>
2026-03-07 13:49:15 +0000

    Add artwork-file API route

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Response;
 use App\Http\Controllers\Api\EntryController;
 
 Route::get('/entries', [EntryController::class, 'index']);
 Route::get('/entries/{id}', [EntryController::class, 'show']);
+
+Route::get('/artwork-file/{file}', function ($file) { // Route to serve artwork files with CORS headers
+   $path = public_path('artworks/' . $file);
+   if (!file_exists($path)) {
+       abort(404);
+   }
+   return Response::file($path, [
+       'Access-Control-Allow-Origin' => '*',
+       'Access-Control-Allow-Methods' => 'GET, OPTIONS',
+       'Access-Control-Allow-Headers' => 'Origin, Content-Type, Accept, Authorization',
+   ]);
+})->where('file', '.*');
